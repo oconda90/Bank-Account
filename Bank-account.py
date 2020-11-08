9a0adf7d488a809bfe43c5f4b6ab657184df10d3
oconda90 <70242240+oconda90@users.noreply.github.com>
2020-11-08 00:26:44 -0800

    Update Bank-account.py
    added a deposit and withdrawl method

--- Bank-account.py
+++ Bank-account.py
@@ -4,4 +4,21 @@ class Account:
         self.AccountNumber = account_number
         self.Route = routing_number
         self.Balance = balance
+
+    def deposit(self, amount):
+        #deposit of money
+        self.Balance = amount + self.Balance
+        print(f'Amount Deposited = ${amount}')
+    
+    def Widthdrawl(self,amount):
+        #The Overdraft fee
+        if amount > self.Balance:
+            self.Balance = self.Balance - amount
+            print("Insufficient Funds")
+            self.Balance = self.Balance - 10 
+            print(f'Amount Withdrawn ${amount}')
+        else:
+            self.Balance =self.Balance - amount
+            print(f'Amount Withdrawn ${amount}')
+    
     
