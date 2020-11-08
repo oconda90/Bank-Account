8cdbe97384550439fef9f07e772b775a5a35627f
oconda90 <70242240+oconda90@users.noreply.github.com>
2020-11-07 23:57:12 -0800

    Update Bank-account.py
    Added a Class named Account

---
+++ Bank-account.py
@@ -0,0 +1,7 @@
+class Account:
+    def __init__(self, full_name, account_number, routing_number, balance):
+        self.Name = full_name
+        self.AccountNumber = account_number
+        self.Route = routing_number
+        self.Balance = balance
+    
